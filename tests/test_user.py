2d75d127a3802f36b1dde3505478d1b5b0ef8280
alansun17904 <asun17904@gmail.com>
2020-10-10 18:01:06 -0400

    user unit tests

---
+++ tests/test_user.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+
+
+def test_foo():
+    assert True
+
+def test_should_create_user_with_username(db) -> None:
+    user = User.objects.create_user("Haki")
+    assert user.username == "Haki"
