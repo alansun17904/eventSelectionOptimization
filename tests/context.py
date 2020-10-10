63562a34d096917861ad026acc4e9fe63c70ef64
alansun17904 <asun17904@gmail.com>
2020-10-10 16:29:28 -0400

    setup profile for heroku and .travis.yml for travis ci

--- tests/context.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
-from source import models
-from source import filter
-from source import settings
